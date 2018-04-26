3114c4e97405a38adb6be487614105f22689429b
Brian Chew <bhchew@ucdavis.edu>
2018-04-26 20:01:05 +0000

    recent

--- main.c
+++ main.c
@@ -3,8 +3,11 @@
 int main(int ac, char **av)
 {
 	FILE *file;
+	unsigned int line_number = 1;
+	int num;
 	char *line;
 	char *token;
+	char *integer;
 	size_t len = 0;
 	ssize_t read_bytes;
 
@@ -23,10 +26,32 @@ int main(int ac, char **av)
 
 	while ((read_bytes = getline(&line, &len, file)) != -1)
 	{
-		token = strtok(line, DELIM)
+		token = strtok(line, DELIM);
 
+		if (token == NULL)
+			continue;
 		printf("%s", token);
+
+                /*if (strcmp(token, "push") == 0)
+			/*call on push function*/
+		/*printf("test");*/
+
+		token = strtok(NULL, DELIM);
+
+		if (token == NULL)
+			break;
+		/*use num as argument for push function*/
+		num = atoi(token);
+		printf("%d", num);
+		if (strcmp(token, itoa(num, integer, 10)) == 0)
+			printf("test");
+
+
+		line_number++;
+		putchar('\n');
 	}
+
+	putchar('\n');
 
 	fclose(file);
 
